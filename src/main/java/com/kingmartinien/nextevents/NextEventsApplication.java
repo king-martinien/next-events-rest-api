16e23739fe58a945d0606bf943898fb4735b43c1
king-martinien <martinienfokoue@yahoo.com>
2024-06-22 03:24:30 +0100

    Initial state

---
+++ src/main/java/com/kingmartinien/nextevents/NextEventsApplication.java
@@ -0,0 +1,13 @@
+package com.kingmartinien.nextevents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextEventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextEventsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kingmartinien/nextevents/NextEventsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kingmartinien.nextevents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextEventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
